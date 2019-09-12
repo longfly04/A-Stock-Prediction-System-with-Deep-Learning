3fc07abf8cc15c699e296f77edf954d8fb60fa9a
LongFly <longfly_04@126.com>
2019-09-12 21:01:04 +0800

    finish tcn and advanced lstm models

--- bin/lstm_model/advanced_lstm_model.py
+++ bin/lstm_model/advanced_lstm_model.py
@@ -1,43 +1,31 @@
+'''
+利用LSTM预测股价，使用全新的config和模型架构。
+
+by LongFly
+
+2019.7
 
 '''
-概述：
-    使用时序卷积网络训练模型，每日股价指标预测未来股价波动，使用膨胀卷积时，模型自动生成深度网络。
-
-TCN：
-    应用扩张卷积、层间残差连接的一维时序卷积网络
-
-Class:
-    Preprocessor:数据预处理器
-    Datamaker:数据切片、划分器
-    Timer:计时器
-    TCN_Model:卷积模型
-    Visualiser:可视化器
-    
-Author:
-    By LongFly
-    2019.09
-'''
-
-import sys,os
-import matplotlib.pyplot as plt
+import os
+import math
 import numpy as np
 import pandas as pd
+import json
 import time
 import datetime as dt
-import json
-
-sys.path.append(
-    'C:\\Users\\longf.DESKTOP-7QSFE46\\GitHub\\A-Stock-Prediction-System-with-GAN-and-DRL')
-
 from numpy import newaxis
-from tcn import TCN
-from sklearn.preprocessing import StandardScaler
-from keras import Input, Model
-from keras.layers import Dense, Flatten
-from keras.callbacks import TensorBoard 
-from keras.models import load_model
+import argparse
+import matplotlib.pyplot as plt
+
+from keras import Model
+from keras.layers import Dense, Activation, Dropout, LSTM, Flatten
+from keras.models import Sequential, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from sklearn.preprocessing import StandardScaler
 from keras import backend as K
+
+import sys
+sys.path.append('C:\\Users\\longf.DESKTOP-7QSFE46\\GitHub\\A-Stock-Prediction-System-with-GAN-and-DRL')
 
 
 class Datapreprocessor():
@@ -295,7 +283,7 @@ class Datamaker():
         i_split = int(len(x_windowed_data) * self.data_config['train_test_split'])
         x_train = x_windowed_data[:i_split:train_window_step]
         y_train = y_windowed_data[:i_split:train_window_step]
-
+        
         batch_size = self.training_config['batch_size']
 
         for i in range(int(len(x_train) / batch_size)):
@@ -326,61 +314,55 @@ class Timer():
         print('Time taken: %s' % (end_dt - self.start_dt))
 
 
-class TCN_Model(Model):
-    '''
-    时序卷积模型：
-        对高维的时序序列卷积
-    '''
+class LSTM_Model(Model):
+    """
+    长短时记忆模型：
+        经典款时序预测模型
+    """
+
     def __init__(self, config, **kwargs):
-        super(TCN_Model, self).__init__(**kwargs)
+        super(LSTM_Model, self).__init__(**kwargs)
         self.model_config = config['model']
         self.training_config = config['training']
         self.data_config = config['data']
+
 
     def load_model(self,):
         filepath = self.training_config['load_model_path']
         print('[Model] Loading model from file %s' % filepath)
         self.model = load_model(filepath)
-    
+
+
     def build_model(self,):
         timer = Timer()
         timer.start()
 
-        batch_size = self.training_config['batch_size']
-        input_timestep = self.data_config['feature_length']
-        input_dim = self.data_config['feature_dim']
-        output_step = self.data_config['target_length']
-        output_dim = self.data_config['target_dim']
-        hyperparas = self.model_config['hyperparameters']
+        self.model = Sequential()
+        for layer in self.model_config['layers']:
+            neurons = layer['neurons'] if 'neurons' in layer else None
+            dropout_rate = layer['rate'] if 'rate' in layer else None
+            activation = layer['activation'] if 'activation' in layer else None
+            return_seq = layer['return_seq'] if 'return_seq' in layer else None
+            input_timesteps = layer['input_timesteps'] if 'input_timesteps' in layer else None
+            input_dim = layer['input_dim'] if 'input_dim' in layer else None
 
-        i = Input(shape=(input_timestep, input_dim))
-        m = TCN(
-            nb_filters=hyperparas['nb_filters'], 
-            kernel_size=hyperparas['kernel_size'], 
-            nb_stacks=hyperparas['nb_stacks'], 
-            dilations=hyperparas['dilations'], 
-            padding=hyperparas['padding'], 
-            use_skip_connections=hyperparas['use_skip_connections'], 
-            dropout_rate=hyperparas['dropout_rate'], 
-            return_sequences=hyperparas['return_sequences'], 
-            activation=hyperparas['activation'], 
-            name=hyperparas['name'], 
-            kernel_initializer=hyperparas['kernel_initializer'], 
-            use_batch_norm=hyperparas['use_batch_norm']
-            )(i)
-        m = Flatten()(m)
-        m = Dense(output_step, activation='linear')(m)
-        
-        if self.data_config['target_mode'] == 'percent' or self.data_config['target_mode'] == 'diff':
-            # 如果target是diff和percent，对输出增加惩罚项 L2 = 1/(1+target^2)，避免模型lasy
-            m = Dense(output_step, activation='linear', activity_regularizer=self.L2_regular)(m)
-        else:
-            m = Dense(output_step, activation='linear')(m)
-        
-        self.model = Model(inputs=[i], outputs=[m])
-        self.model.summary()
+            if layer['type'] == 'dense':
+                if self.data_config['target_mode'] == 'percent' or self.data_config['target_mode'] == 'diff':
+                    # 如果target是diff和percent，对输出增加惩罚项 L2 = 1/(1+target^2)，避免模型lasy
+                    self.model.add(Dense(neurons, activation=activation, activity_regularizer=self.L2_regular))
+                else:
+                    self.model.add(Dense(neurons, activation=activation))
+
+            if layer['type'] == 'lstm':
+                self.model.add(LSTM(neurons, input_shape=(input_timesteps, input_dim), return_sequences=return_seq))
+            if layer['type'] == 'dropout':
+                self.model.add(Dropout(dropout_rate))
+            if layer['type'] == 'flatten':
+                self.model.add(Flatten())
+
         self.model.compile(loss=self.model_config['loss'], optimizer=self.model_config['optimizer'])
-        
+        self.model.summary()
+        # 模型使用了三层LSTM网络，最后的LSTM 将序列输出到全连接层。
         if self.model_config['plot_model']:
             from keras.utils import plot_model
             plot_model(self.model, to_file=self.model_config['plot_model_path'])
@@ -393,19 +375,19 @@ class TCN_Model(Model):
         timer.start()
         print('[Model] Training Started')
         print('[Model] %s epochs, %s batch size' % (self.training_config['epochs'], self.training_config['batch_size']))
-        
-        save_fname = os.path.join(self.training_config['save_dir'], 
-                                  '%s-epochs%s.h5' % (dt.datetime.now().strftime('%Y%m%d-%H%M%S'), 
-                                  str(self.training_config['epochs'])))
+        # 模型的参数保存下来
+        save_fname = os.path.join(self.training_config['save_dir'],
+                                 '%s-e%s.h5' % (dt.datetime.now().strftime('%Y%m%d-%H%M%S'), 
+                                 str(self.training_config['epochs'])))
         callbacks = [
-            EarlyStopping(monitor='val_loss', patience=3),
+            EarlyStopping(monitor='val_loss', patience=2),
             ModelCheckpoint(filepath=save_fname, monitor='val_loss', save_best_only=True),
             TensorBoard(log_dir=self.training_config['tensorboard_dir'])
         ]
 
         x = x[x.shape[0]%self.training_config['batch_size']:]
         y = y[y.shape[0]%self.training_config['batch_size']:]
-        # 对于数据不是batch size的整数倍问题，抛弃数据集最前的盈余数据,因为时间序列越接近预测目标的数据越有用
+
         self.history = self.model.fit(
                                       x,
                                       y,
@@ -413,7 +395,7 @@ class TCN_Model(Model):
                                       batch_size=self.training_config['batch_size'],
                                       callbacks=callbacks,
                                       validation_split=self.training_config['validation_split'],
-                                      )
+        )
         if self.training_config['save']:
             if not os.path.exists(self.training_config['save_dir']): 
                 os.makedirs(self.training_config['save_dir'])
@@ -458,9 +440,59 @@ class TCN_Model(Model):
 
     def predict_multistage(self, x_test):
         predictions = []
-        for x_test_i in x_test[:,:,:]:
+        for x_test_i in x_test:
             predictions.append(self.model.predict(x_test_i[newaxis, :, :]))
         return predictions
+
+
+    def predict_sequences_multiple(self, x_test, window_size, prediction_len):
+        #预测test_len/(seq_len * predict_window)个时间步的股价 这个预测跟训练的时间步吻合 并且有一定的实际应用价值
+        print('[Model] Predicting Sequences Multiple...')
+        prediction_seqs = []
+        for i in range(int(len(x_test))):
+            curr_slice = x_test[i]
+            prediction_seqs.append(self.model.predict(curr_slice[newaxis,:,:]))
+        prediction = np.array(prediction_seqs).reshape(prediction_len * len(prediction_seqs))
+        prediction = self.rectify_predict(prediction, window_size=window_size)
+        return prediction
+
+    def predict_sequence_overlap(self, x_test, window_size):
+        # 使用重叠的时间窗口预测股价，并在重叠处使用平均值削弱噪声的影响
+        print('[Model] Predicting Sequences Average...')
+        # 记录预测结果
+        prediction_seqs = []
+
+        for i in range(int(len(x_test))):
+            curr_slice = x_test[i, :, :]
+            predicted = self.model.predict(curr_slice[newaxis,:,:])
+            prediction_seqs.append(predicted)
+        prediction_seqs = np.array(prediction_seqs).reshape(int(len(x_test)), window_size)
+        prediction_matrix = np.zeros([int(len(x_test)), int(len(x_test))+window_size-1]) 
+        # 将窗口数据按照实际的时间起始点平移赋值给0矩阵，下一步进行按列求和取平均
+        for i in range(int(len(x_test))):
+            prediction_matrix[i, i:i+window_size] = prediction_seqs[i, :]
+        # 对矩阵按列相加
+        prediction = prediction_matrix.sum(axis=0)
+        # 取平均值 前window_size和后window_size个数据的分母是变化的 中间都是除以window_size
+        for i in range(int(len(prediction))):
+            if i >= window_size and i <= len(prediction)-window_size:
+                prediction[i] = prediction[i]/window_size
+            elif i < window_size:
+                prediction[i] = prediction[i]/(i+1)
+            elif i > len(prediction)-window_size:
+                prediction[i] = prediction[i]/(len(prediction)-i)
+        return prediction
+
+    def rectify_predict(self, prediction, window_size):
+        # 对窗口预测的股价进行修正 防止突破涨跌停板限制
+        for i in range(int(len(prediction))-1):
+            if prediction[i+1] > prediction[i] * 1.1 :
+                # 如果预测结果突破了涨停板限制 将后面一个窗口的数据同时赋值 相当于在这个窗口衔接处经历了一个涨停板
+                prediction[i+1:i+window_size] = prediction[i+1:i+window_size] - (prediction[i+1] - prediction[i] * 1.1)
+            elif prediction[i+1] < prediction[i] * 0.9 :
+                # 如果预测结果突破了跌停板限制 将后面一个窗口的数据同时赋值 相当于在这个窗口衔接处经历了一个跌停板
+                prediction[i+1:i+window_size] = prediction[i+1:i+window_size] + (prediction[i] * 0.9 - prediction[i+1])
+        return prediction
 
 
 class Visualiser():
@@ -530,9 +562,10 @@ class Visualiser():
 
 
     def plot_predictions(self,):
-        fig = plt.figure()
-        plt.title(self.visual_config['title'])
-        plt.plot(self.plot_actual, label='True Data')
+        fig = plt.figure(facecolor='white')
+        ax = fig.add_subplot(111)
+        fig.title(self.visual_config['title'])
+        ax.plot(self.plot_actual, label='True Data')
         plt.plot(self.plot_predict, label='Prediction')
         plt.legend()
         plt.show()
@@ -548,8 +581,134 @@ class Visualiser():
         plt.show()
 
 
+def plot_results(predicted_data, true_data, prediction_len):
+    plot_predicted = predicted_data
+    plot_true = true_data
+
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(plot_true, label='True Data')
+    plt.plot(plot_predicted, label='Prediction')
+    plt.legend()
+    plt.show()
+
+def get_data_statistics(data, fill_inf=False):# 获取数据的统计信息 默认不对无穷数进行处理
+
+    indicators = { # 所有的数据指标名称与对应指标类型
+        'daily':'日线行情',
+        'daily_indicator':'每日指标',
+        'moneyflow': '个股资金流向',
+        'res_qfq':'前复权行情',
+        'res_hfq': '后复权行情',
+        'income': '利润表',
+        'balancesheet': '资产负债表',
+        'cashflow': '现金流量表',
+        'forecast': '业绩预告',
+        'express': '业绩快报',
+        'dividend': '分红送股',
+        'financeindicator': '财务指标',
+        'HSGTflow': '沪深港通资金流向',
+        'margin': '融资融券交易汇总',
+        'pledge': '股权质押统计',
+        'repurchase': '股票回购',
+        'desterilization': '限售股解禁',
+        'block': '大宗交易',
+        'shibor': '上海银行间同业拆放利率',
+        'shiborquote': '上海银行间同业拆放利率报价汇总',
+        'shiborLPR': '贷款基础利率',
+        'libor': '伦敦同业拆借利率',
+        'hibor': '香港银行同行业拆借利率',
+        'wen': '温州民间融资综合利率指数',
+        'tech': '技术分析',
+        'fft': '傅里叶变换',
+    } 
+
+    print('1.数据集共有{}个样本，{}个特征。'.format(data.shape[0], data.shape[1]))
+
+    print('2.数据集基本信息：')
+    print(data.describe())
+
+    print('3.数据集特征和数据类型情况：')
+    datatype = pd.DataFrame({'Feature Names':data.columns, 'Data Types':str(data.dtypes)})
+    indicators_idx = {} # 指标索引 找到数据集中不同类别指标的位置
+    indicators_count = 0
+    for key in indicators:
+        indicators_idx[key+'_idx'] = [f.startswith(key+'_') for f in datatype['Feature Names'].tolist()]
+        print(indicators[key] + ' 特征数量为：' + str(indicators_idx[key+'_idx'].count(True)) + '个 ')
+        indicators_count = indicators_count + indicators_idx[key+'_idx'].count(True)
+    # 总的特征数减去daily_indicator 的特征数 因为这个特征在daily中已经包含了
+    indicators_count = indicators_count - indicators_idx['daily_indicator_idx'].count(True)
+    print('有标记特征数量合计{}个，其他特征{}个'.format(indicators_count, data.shape[1]-indicators_count))
+
+    print('4.数据集包含空值情况统计：')
+    print(data.isna().sum().sum())
+
+    print('5.数据集无穷数情况统计：')
+    # 获取特征名称的np数组
+    feature_name = np.array(data.columns).astype(str)
+    # 获取特征取值的np数组
+    values = np.array(data.values).astype(float)
+    # 获取存在无穷数据的索引
+    idx = np.where(np.isinf(values)==True)
+    # 将无穷数据的索引zip成坐标
+    idx_list = list(zip(idx[0], idx[1]))
+
+    while len(idx_list) > 0:
+        print('数据集中共包含{}个无穷数'.format(len(idx_list)))
+        print('以下特征出现了无穷数：')
+        # 获取出现了无穷数据的特征名称的索引
+        feature_idx = set(idx[1])
+        # 将每个特征列 除了无穷数据之外的数据的最大值 保存成字典
+        feature_max = {}
+        for _ in feature_idx:
+            print(feature_name[_])
+            # 将当前特征列的有效数据取出来 并计算最大值 
+            # 在显示特征名称这个循环中顺便处理这个列的无穷数据
+            # 首先获得有效数据的索引
+            idx_significant = np.where(np.isinf(values[:,_])==False)
+            # 特征最大值的字典 key为特征名称 value为最大值
+            feature_max[feature_name[_]] = data[feature_name[_]].iloc[idx_significant].max()
+        print('无穷数的索引为：')
+        print(idx_list)
+        if fill_inf:# 对无穷数进行处理 用当前列数据中的 最大值 或者用0 来填充
+            for i in idx_list:
+                if np.isinf(data.iloc[i]):
+                    data.iloc[i] = feature_max[feature_name[i[1]]]
+            print('已将无穷数填充为特征列最大值。')
+            # 再次检验是否有无穷数
+            values = np.array(data.values).astype(float)
+            idx = np.where(np.isinf(values)==True)
+            idx_list = list(zip(idx[0], idx[1]))
+
+    print('数据集中无无穷数。')
+    return data
+
+def read_data(file_name):# 通过文件路径读取文件 并处理数据中的空值和无意义的列
+    # 读取已经进行过特征工程的数据
+    data_csv = pd.read_csv(file_name)
+    data = data_csv.drop(columns=['Unnamed: 0', 'Unnamed: 0.1', ])
+    # 将交易日期设置为索引 便于分析和画图
+    cal_date = pd.to_datetime(data['cal_date'], format='%Y%m%d').to_list()
+    data = pd.DataFrame(data={col:data[col].tolist() for col in data.columns}, index=cal_date)
+    # 获取包含有指定名称的列名索引
+    cols = list(data.columns.astype(str))
+    td_col = [col for col in cols if col.endswith('trade_date') or col.endswith('ann_date') or col.endswith('cal_date') or col.endswith('end_date')]
+    # 删除指定列
+    data = data.drop(columns=list(td_col))
+    # 对nan数据填0
+    data = data.fillna(0)
+
+    return data
+
+def parse_args(): # 处理参数 分别是加载已经保存好的模型的路径 以及预测值的模式：滑动窗口还是多窗口
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--loadfile", default="", help="input the path of the saved model.")
+    parser.add_argument("--predict_mode", default="multi", help="input the mode of the prediction? multi/avg")
+    args = parser.parse_args()
+    return args
+
 def main():
-    config = json.load(open('bin\\tcn_model\\tcn_config.json', 'r', encoding='utf-8'))
+    config = json.load(open('bin\\lstm_model\\advanced_lstm_config.json', 'r', encoding='utf-8'))
 
     # 定义一个预处理器
     preprocessor = Datapreprocessor(config=config)
@@ -559,22 +718,22 @@ def main():
     datamaker = Datamaker(config=config, data=data)
     [x_windowed_data, y_windowed_data] = datamaker.window_data()
     [x_train, y_train, x_test, y_test] = datamaker.split_data(x_windowed_data, y_windowed_data)
+    data_generator = datamaker.generate_data(x_windowed_data, y_windowed_data)
+    steps = int(x_train.shape[0]/config['training']['batch_size'])
     
     # 加载已有模型或者训练模型
     if config['training']['load']:
         # 如果从已保存的模型中加载
-        model = TCN_Model(config=config)
+        model = LSTM_Model(config=config)
         model.load_model()
     elif config['training']['generative']:
         # 使用生成器训练
-        data_generator = datamaker.generate_data(x_windowed_data, y_windowed_data)
-        steps = int(x_train.shape[0]/config['training']['batch_size'])
-        model = TCN_Model(config=config)
+        model = LSTM_Model(config=config)
         model.build_model()
         model.train_generate_model(data_gen=data_generator, steps=steps)
         # 使用生成器训练，需要指定训练集生成器、验证集生成器和训练一个epoch的步数
     else:
-        model = TCN_Model(config=config)
+        model = LSTM_Model(config=config)
         model.build_model()
         model.train_model(x_train, y_train)
 
@@ -584,20 +743,12 @@ def main():
     visualiser = Visualiser(config=config, 
                             prediction=predictions, 
                             actuality=y_test, 
-                            history=None if config['training']['load'] else model.history
+                            history=model.history
                             )
-    visualiser.preprocess()
-    # 先对预测结果进行预处理，再绘制预测结果和训练过程示意图
-    if config['visualise']['plot_prediction']:
-        visualiser.plot_predictions() 
-    if config['visualise']['plot_loss']:
-        visualiser.plot_history()
 
+    # 绘制预测结果和训练过程示意图
+    visualiser.plot_predictions()
+    visualiser.plot_history()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
-
-
-
